7c51bc8f6a4071f0579abc5c548cba4c823b5dae
Muntasir Raihan Rahman <160683781+mrrahman1517@users.noreply.github.com>
2026-03-30 08:38:11 -0700

    Add: lowest-common-ancestor-in-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-2.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // if p->val < root <  q->val, p must be in left substree and q in right subtree, so root is the LCA
+        // if q->val < root < p->val, q must be in left subtree and p must be in right subtree, so the root is the LCA
+        // if both p and q are greater than root, search in the right subtree
+        // if both p and q are lower than root, search in the left subtree 
+        // if one of them is the root, then it has to be the lca
+        /*if (root == p || root == q) {
+            return root;
+        }
+        if (p->val < root->val && root->val < q->val) {
+            return root;
+        }
+        if (q->val < root->val && root->val < p->val) {
+            return root;
+        }*/
+        if (p->val < root->val && q->val < root->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        else if (p->val > root->val && q->val > root->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        else {
+            return root;
+        }
+
+    }
+};
