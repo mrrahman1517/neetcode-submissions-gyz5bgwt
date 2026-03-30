128a90d22e8936e03514e3044bb4b02ec0453abc
Muntasir Raihan Rahman <160683781+mrrahman1517@users.noreply.github.com>
2026-03-30 08:40:40 -0700

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<bool> visited;
+    vector<vector<int>> graph;
+
+    void dfs(int u) {
+        visited[u] = true;
+        for (auto& v: graph[u]) {
+            if (!visited[v]) {
+                dfs(v);
+            }
+        }
+    }
+
+    int countComponents(int n, vector<vector<int>>& edges) {
+        if (n == 1) {
+            return 1;
+        }
+        graph.resize(n);
+        //visited = vector<bool>(n);
+        visited.resize(n);
+        for (int i = 0; i < n; i++) {
+            graph[i] = vector<int>(n);
+        }
+        for (auto& edge: edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+        return count;
+    }
+};
