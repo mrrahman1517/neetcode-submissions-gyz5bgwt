fdd829cdb749421f60d0bf335681855dc51ecc75
Muntasir Raihan Rahman <160683781+mrrahman1517@users.noreply.github.com>
2026-03-30 08:44:15 -0700

    Add: non-overlapping-intervals - submission-3

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # idea DP
+        # sort by start value
+        # store first interval, and its end value
+        # for each new interval, test for overlap
+        # if overlap, delete interval with max end value, update max end value
+        # greedy so local decisions should be optimal
+        if not intervals:
+            return 0
+        intervals.sort(key = lambda x : x[0])
+        endmax = intervals[0][1]
+        count = 0
+        for i in range(1, len(intervals)):
+            # if overlap
+            if intervals[i][0] >= endmax:  
+                # no overlap, update max endpoint  
+                endmax = intervals[i][1] # end point is always after start point
+            else:
+                # overlap, ignore interval with greater endpoint
+                count += 1
+                endmax = min(endmax, intervals[i][1])
+        return count
+        
