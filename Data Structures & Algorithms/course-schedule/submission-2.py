c023e3e9086c70dd9635048eff5e2c45f34af4ee
Muntasir Raihan Rahman <160683781+mrrahman1517@users.noreply.github.com>
2026-03-30 08:40:01 -0700

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list[int])
+        indeg = [0] * numCourses
+        
+        # construct DAG from input
+
+        for (u, v) in prerequisites:
+            graph[v].append(u)
+            indeg[u] += 1
+        
+        q = deque([node for node in range(numCourses) if indeg[node] == 0])
+        #for node in range(numCourses):
+        #    if indeg[node] == 0:
+        count = 0
+        while q:
+            u = q.popleft()
+            count += 1
+            for v in graph[u]:
+                indeg[v] -= 1 # edge (uv) implies indeg(v) > 0
+                if indeg[v] == 0:
+                    q.append(v)
+
+
+        return count == numCourses
