2b87e24e20e41523c613dd8838c5c6534d4d31dd
Muntasir Raihan Rahman <160683781+mrrahman1517@users.noreply.github.com>
2026-03-30 08:40:14 -0700

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        p = [x for x in range(n)]
+        sz = [1 for _ in range(n)]
+        #self.count = n
+        # use edge and disjoint set data structure
+        def find(x):
+            while x != p[x]:
+                p[x] = p[p[x]]
+                x = p[x]
+            return x
+        def union(u, v):
+            pu = find(u)
+            pv = find(v)
+            if pu == pv:
+                return False
+            if sz[pu] <= sz[pv]:
+                # pu smaller tree, make parent(pu) = pv
+                p[pu] = pv
+                sz[pv] += sz[pu]
+            else:
+                p[pv] = pu
+                sz[pu] += sz[pv]
+            return True
+
+        for (u, v) in edges:
+            if not union(u, v):
+                return False
+        return True
